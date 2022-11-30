051d2b32ab01c87256286840ef8a07f7c29261d6
Kevin <kpogrund@gmail.com>
2022-11-30 16:44:07 +0200

    Complete Permutation.java

--- 3.queues/Permutation.java
+++ 3.queues/Permutation.java
@@ -1,2 +1,14 @@
+import edu.princeton.cs.algs4.StdIn;
+
 public class Permutation {
+    public static void Main(String[] args){
+        RandomizedQueue<String> randomizedQueue = new RandomizedQueue<String>();
+        while (!StdIn.isEmpty()){
+            randomizedQueue.enqueue(StdIn.readString());
+        }
+        for (int i = 0; i < Integer.parseInt(args[0]); i++) {
+            System.out.println(randomizedQueue.dequeue());
+        }
+    }
+
 }
