cfac192f8997c75f5ab3012239fd72cee175108a
Kevin <kpogrund@gmail.com>
2022-11-30 16:48:26 +0200

    changed Main to main in Permutation.java

--- 3.queues/Permutation.java
+++ 3.queues/Permutation.java
@@ -1,7 +1,7 @@
 import edu.princeton.cs.algs4.StdIn;
 
 public class Permutation {
-    public static void Main(String[] args){
+    public static void main(String[] args){
         RandomizedQueue<String> randomizedQueue = new RandomizedQueue<String>();
         while (!StdIn.isEmpty()){
             randomizedQueue.enqueue(StdIn.readString());
