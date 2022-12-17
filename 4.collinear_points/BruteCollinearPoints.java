a3a2e24911dda50ebc1376cb2cbdab04f7116a43
Kevin Pogrund <kpogrund@gmail.com>
2022-12-17 21:45:42 +0200

    Changed the start point of the nested loops in BruteCollinearPoints.java

--- 4.collinear_points/BruteCollinearPoints.java
+++ 4.collinear_points/BruteCollinearPoints.java
@@ -15,16 +15,16 @@ public class BruteCollinearPoints {
 
         Arrays.sort(this.points);
 
-        for (int i = 0; i < this.points.length-3; i++) {
-            for (int j = 0; j < this.points.length-2; j++) {
-                for (int k = 0; k < this.points.length-1; k++) {
-                    for (int l = 0; l < this.points.length; l++) {
+        for (int i = 0; i < this.points.length - 3; i++) {
+            for (int j = i + 1; j < this.points.length - 2; j++) {
+                for (int k = j + 1; k < this.points.length - 1; k++) {
+                    for (int m = k + 1; m < this.points.length; m++) {
                         double iToJ = this.points[i].slopeTo(this.points[j]);
                         double jToK = this.points[j].slopeTo(this.points[k]);
-                        double kToL = this.points[k].slopeTo(this.points[l]);
-                        if(iToJ==jToK&& jToK==kToL){
-                            enqueue(new LineSegment(this.points[i], this.points[l]));
-                            this.points[i].drawTo(this.points[l]);
+                        double kToL = this.points[k].slopeTo(this.points[m]);
+                        if (iToJ == jToK && jToK == kToL) {
+                            enqueue(new LineSegment(this.points[i], this.points[m]));
+                            this.points[i].drawTo(this.points[m]);
                             StdDraw.show();
                         }
                     }
@@ -36,25 +36,26 @@ public class BruteCollinearPoints {
 
     }    // finds all line segments containing 4 points
 
-    private void enqueue(LineSegment seg){
-        if(seg==null) throw new IllegalArgumentException();
-        if(this.numberOfSegments ==this.segments.length) resize(2*this.segments.length);
-        this.segments[this.numberOfSegments++]=seg;
+    private void enqueue(LineSegment seg) {
+        if (seg == null) throw new IllegalArgumentException();
+        if (this.numberOfSegments == this.segments.length) resize(2 * this.segments.length);
+        this.segments[this.numberOfSegments++] = seg;
     }
 
-    private void resize(int size){
-        assert size>= this.numberOfSegments;
+    private void resize(int size) {
+        assert size >= this.numberOfSegments;
         LineSegment[] temp = new LineSegment[size];
         System.arraycopy(this.segments, 0, temp, 0, this.numberOfSegments);
         this.segments = temp;
     }
+
     public int numberOfSegments() {
         return this.numberOfSegments;
     }    // the number of line segments
 
     private void checkPoints(Point[] point) {
 
-        if (point == null) throw new NullPointerException();
+        if (point == null) throw new IllegalArgumentException();
         for (int i = 0; i < point.length; i++) {
             if (point[i] == null) throw new IllegalArgumentException();
             for (int j = 0; j < point.length; j++) {
@@ -64,7 +65,7 @@ public class BruteCollinearPoints {
     }
 
     public LineSegment[] segments() {
-        return Arrays.copyOf(this.segments,this.numberOfSegments);
+        return Arrays.copyOf(this.segments, this.numberOfSegments);
     }     // the line segments
 
 
